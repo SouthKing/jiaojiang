586f95c7eb56465296aad08be79f37519f2595f2
Min Zhang <mizhang@akamai.com>
2013-08-27 20:57:16 -0400

    20130827

--- src/jiaojiang/leetcode/mainpage/SlidingWindowMax.java
+++ src/jiaojiang/leetcode/mainpage/SlidingWindowMax.java
@@ -21,12 +21,54 @@ public class SlidingWindowMax {
     Output: An array B[], B[i] is the maximum value of from A[i] to A[i+w-1]
     Requirement: Find a good optimal way to get B[i]\
      */
-    public static int[] slidingMax(int[] a, int w) {
-        return null;
+    public static int[] slidingMax(int[] data, int w) {
+        int[] ret = new int[data.length - w  + 1];
+        int[] dq = new int[w]; //double queue
+        int dqTail = 0;
+
+        for (int i = 0; i < w - 1; i++) {
+            while (dqTail > 0 && data[i] > dq[dqTail - 1]) {
+                dqTail--;
+            }
+            dq[dqTail++] = data[i];
+        }
+
+        for (int i = w - 1; i < data.length; i++) {
+            while (dqTail > 0 && data[i] > dq[(dqTail - 1)% w]) {
+                dqTail--;
+            }
+            dq[dqTail++%w] = data[i];
+            ret[i - w + 1] = dq[dqTail >= w ? dqTail % w : 0];
+        }
+
+        return ret;
     }
+    
 
     public static void test() {
+        for (int d : slidingMax(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)) {  // 3, 3, 5, 5, 6, 7
+            System.out.print(d + "\t");
+        }
+        System.out.println("\n");
 
+        for (int d : slidingMax(new int[]{1, 5, 10, -3, 5, 13, 6, 7}, 3)) { //10, 10, 10, 13, 13, 13
+            System.out.print(d + "\t");
+        }
+        System.out.println("\n");
+
+        for (int d : slidingMax(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 3)) { //3, 4, 5, 6, 7, 8
+            System.out.print(d + "\t");
+        }
+        System.out.println("\n");
+
+        for (int d : slidingMax(new int[]{8, 7, 6, 5, 4, 3, 2, 1}, 3)) { //8, 7, 6, 5, 4, 3
+            System.out.print(d + "\t");
+        }
+        System.out.println("\n");
+    }
+
+    public static void main(String[] args) {
+        test();
     }
 
 }
