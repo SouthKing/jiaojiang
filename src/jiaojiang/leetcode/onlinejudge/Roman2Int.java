3d7061abe400fbd6759f15154b32097209472da5
gatechang <gatechang@gmail.com>
2013-09-24 17:01:51 -0400

    wordsegment

--- src/jiaojiang/leetcode/onlinejudge/Roman2Int.java
+++ src/jiaojiang/leetcode/onlinejudge/Roman2Int.java
@@ -11,11 +11,38 @@ public class Roman2Int {
     Input is guaranteed to be within the range from 1 to 3999.
     */
     public static int romanToInt(String s) {
-        return 0;
+        int ret = 0;
+        for (int i = 0, n = s.length(); i < n; i++) {
+            char c = s.charAt(i);
+            int next, current = romanDigit2Int(c);
+            if ( i + 1 < n && current < (next = romanDigit2Int(s.charAt(i + 1))) ) {
+                ret += next - current;
+                i++;
+            } else {
+                ret += current;
+            }
+        }
+        return ret;
+    }
+
+    private static int romanDigit2Int(char c) {
+        switch (c) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default: return 0;
+        }
     }
 
     public static void test() {
         System.out.println("get: " + romanToInt("I") + ", expected: 1");
+        System.out.println("get: " + romanToInt("IV") + ", expected: 4");
+        System.out.println("get: " + romanToInt("CCVII") + ", expected: 207");
+        System.out.println("get: " + romanToInt("MLXVI") + ", expected: 1066");
     }
 
     public static void main(String[] args) {
