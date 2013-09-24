fc4e7c2208ac80cbb55c653595c67160780699bc
gatechang <gatechang@gmail.com>
2013-09-24 15:35:38 -0400

    adding 4 sum

--- src/jiaojiang/leetcode/onlinejudge/FourSum.java
+++ src/jiaojiang/leetcode/onlinejudge/FourSum.java
@@ -1,6 +1,7 @@
 package jiaojiang.leetcode.onlinejudge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author: mizhang
@@ -23,8 +24,56 @@ public class FourSum {
         (-2,  0, 0, 2)
      */
     public static ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+        Arrays.sort(num);
+        
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < num.length - 3; ) {
+            for (int j = i + 1; j < num.length - 2; ) {
+                int start = j + 1, end = num.length - 1;
+                while (start < end) {
+                    if (num[start] + num[end] == target - num[i] - num[j]) {
+                        ArrayList<Integer> list = new ArrayList<Integer>();
+                        list.add(num[i]);
+                        list.add(num[j]);
+                        list.add(num[start]);
+                        list.add(num[end]);
+                        ret.add(list);
+                        while (++start < end && num[start - 1] == num[start]);
+                        while (start < --end && num[end] == num[end + 1]);
+                    } else if (num[start] + num[end] < target - num[i] - num[j]) {
+                        start++;
+                    } else {
+                        end--;
+                    }
+                }
+                while (++j < num.length - 2 && num[j - 1] == num[j]);
+            }
+            while(++i < num.length - 3 && num[i - 1] == num[i]);
+        }
 
-        return null;
+        return ret;
+    }
+
+    private static ArrayList<ArrayList<Integer>> twoSum(int[] num, int target, int start, int end) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+
+        int i = start, j = end;
+        while (i < j) {
+            if (num[i] + num[j] == target) {
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                list.add(num[i]);
+                list.add(num[j]);
+                ret.add(list);
+                i++;
+                j--;
+            } else if (num[i] + num[j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return ret;
     }
 
     public static void test(int[] num, int target) {
@@ -38,6 +87,8 @@ public class FourSum {
 
     public static void test() {
         test(new int[] {1, 0, -1, 0, -2, 2}, 0);
+        System.out.println("=========================");
+        test(new int[] {-3, -2, -1, 0, 0, 1, 2, 3}, 0);//[[-3,-2,2,3],[-3,-1,1,3],[-3,0,0,3],[-3,0,1,2],[-2,-1,0,3],[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
     }
 
     public static void main(String[] args) {
