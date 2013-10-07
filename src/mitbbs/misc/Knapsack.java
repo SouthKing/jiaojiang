0cc8e4bb3fbef2a14096c79ed6454d7ce5c4ca27
gatechang <gatechang@gmail.com>
2013-10-06 22:55:29 -0400

    20132255

--- src/mitbbs/misc/Knapsack.java
+++ src/mitbbs/misc/Knapsack.java
@@ -1,5 +1,7 @@
 package mitbbs.misc;
 
+import jiaojiang.utils.Utils;
+
 /**
  * @author: zhang
  * @since: Oct 6, 2013 12:11:37 AM
@@ -10,11 +12,14 @@ value
  */
 public class Knapsack {
     public static int maxValue(int[] weights, int[] values, int maxWeight) {
+        
+
         return 0;
     }
 
     public static void test() {
-
+        Utils.println(maxValue(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 9, 13, 15}, 5), 16);
+        Utils.println(maxValue(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 9, 13, 15}, 7), 22);
     }
 
     public static void main(String[] args) {
