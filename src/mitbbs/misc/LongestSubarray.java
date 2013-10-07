aa6fb5388ceeafe7f23bef8cd96bf684dee3dc55
gatechang <gatechang@gmail.com>
2013-10-06 22:24:31 -0400

    longest repeating subarray in sorted arrary

--- src/mitbbs/misc/LongestSubarray.java
+++ src/mitbbs/misc/LongestSubarray.java
@@ -16,17 +16,54 @@ Find the longest subarray with repeating numbers, i.e., 333333333333
 public class LongestSubarray {
 
     public static int[] longestRepeatingSubarray(int[] sortedData) {
-        int[] ret = new int[2];
-        
+        int start = 0, maxLength = 0, maxStart = 0;
 
-        return ret;
+        while (start < sortedData.length) {
+            int end = binarySearch(sortedData, start);
+            if (end - start > maxLength) {
+                maxLength = end - start;
+                maxStart = start;
+            }
+            start = end;
+        }
+
+        return new int[]{maxStart, maxLength};
+    }
+
+    /*
+    return the index of the first next integer after sortedData[index]
+     */
+    private static int binarySearch(int[] sortedData, int index) {
+        int start = index, end = sortedData.length - 1, mid;
+        while (start <= end) {
+            mid = (start + end) >>> 1;
+            if (sortedData[mid] > sortedData[index] && mid - 1 >= 0 && sortedData[mid - 1] == sortedData[index]) {
+                return mid;
+            }
+
+            if (sortedData[mid] == sortedData[index]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return sortedData.length;
+    }
+
+    private static void testBS(){
+        System.out.println(binarySearch(new int[]{0, 0, 0, 1, 1, 1, 4, 5, 6, 7, 7, 7, 7, 7}, 9)); // 14
+        System.out.println(binarySearch(new int[]{0, 0, 0, 1, 1, 1, 4, 5, 6, 7, 7, 7, 7, 7}, 3)); // 6
     }
 
     public static void test() {
         Utils.printIntArrayln(longestRepeatingSubarray(new int[]{0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}), new int[]{13, 12});
+        Utils.printIntArrayln(longestRepeatingSubarray(new int[]{0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3}), new int[]{7, 6});
+        Utils.printIntArrayln(longestRepeatingSubarray(new int[]{0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 3, 3, 3, 3}), new int[]{0, 7});
     }
 
     public static void main(String[] args) {
+//        testBS();
         test();
     }
 }
