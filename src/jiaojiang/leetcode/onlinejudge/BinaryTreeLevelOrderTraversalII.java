b1f05798ae569e326706458405b185504818536c
gatechang <gatechang@gmail.com>
2013-10-09 16:05:17 -0400

    level traversal II

---
+++ src/jiaojiang/leetcode/onlinejudge/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,35 @@
+package jiaojiang.leetcode.onlinejudge;
+
+import java.util.ArrayList;
+
+/**
+ * @author: mizhang
+ * @since: Oct 9, 2013 4:04:23 PM
+ */
+public class BinaryTreeLevelOrderTraversalII {
+    /*
+    Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+    For example:
+    Given binary tree {3,9,20,#,#,15,7},
+
+        3
+       / \
+      9  20
+        /  \
+       15   7
+
+    return its bottom-up level order traversal as:
+
+    [
+      [15,7]
+      [9,20],
+      [3],
+    ]
+     */
+    public static ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+
+        return null;
+    }
+    
+}
