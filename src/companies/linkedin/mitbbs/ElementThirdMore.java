6c9f7e923c81cf6d944e6556b1e0cf64ec9a5303
Min Zhang <mizhang@akamai.com>
2013-10-30 17:21:36 -0400

    linkedin

---
+++ src/companies/linkedin/mitbbs/ElementThirdMore.java
@@ -0,0 +1,26 @@
+package companies.linkedin.mitbbs;
+
+import utils.Utils;
+
+/**
+ * @author: mizhang
+ * @since: Oct 30, 2013 4:55:17 PM
+ */
+public class ElementThirdMore {
+    /*
+    Given an array, one element occurs more than N/3 times, where N is the length of the array. Find this element.
+     */
+
+    public static int find(int[] elements) {
+
+        return 0;
+    }
+
+    public static void test() {
+        Utils.printTestln(find(new int[]{6, 2, 2, 6, 3, 4, 6, 6, 6, 6}), 6);
+    }
+    
+    public static void main(String[] args) {
+        test();
+    }
+}
