541d3d151a84e3d9f9f62a9f97ea00a3a1866e50
gatechang <gatechang@gmail.com>
2013-10-30 20:34:05 -0400

    1/3 more

--- src/companies/linkedin/mitbbs/ElementThirdMore.java
+++ src/companies/linkedin/mitbbs/ElementThirdMore.java
@@ -12,12 +12,32 @@ public class ElementThirdMore {
      */
 
     public static int find(int[] elements) {
+        int[] count = new int[2];
+        int[] element = new int[2];
 
-        return 0;
+        for (int i : elements) {
+            if (count[0] == 0 && count[1] == 0 || count[0] == 0 && element[1] != i) {
+                count[0]++;
+                element[0] = i;
+            } else if (count[1] == 0 && element[0] != i) {
+                element[1] = i;
+            } else if (count[0] != 0 && element[0] == i) {
+                count[0]++;
+            } else if (count[1] != 0 && element[1] == i) {
+                count[1]++;
+            } else {
+                count[0]--;
+                count[1]--;
+            }
+        }
+
+        return count[0] == 0 ? element[1] : element[0];
     }
 
     public static void test() {
         Utils.printTestln(find(new int[]{6, 2, 2, 6, 3, 4, 6, 6, 6, 6}), 6);
+        Utils.printTestln(find(new int[]{6, 2, 2, 4, 3, 4, 3, 6, 6, 6}), 6);
+        Utils.printTestln(find(new int[]{4, 5, 6, 7, 8, 4, 4}), 4);
     }
     
     public static void main(String[] args) {
