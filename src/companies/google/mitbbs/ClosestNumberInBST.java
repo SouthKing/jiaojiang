8614845015a9e0de53bb83bdfce170156d3aad9c
gatechang <gatechang@gmail.com>
2013-10-31 17:14:55 -0400

    google interview

---
+++ src/companies/google/mitbbs/ClosestNumberInBST.java
@@ -0,0 +1,19 @@
+package companies.google.mitbbs;
+
+import utils.BST;
+
+/**
+ * @author: mizhang
+ * @since: Oct 31, 2013 4:50:29 PM
+ */
+public class ClosestNumberInBST {
+    /*
+    Given a number and a BST, find the closest number in BST, by closest, means the absolute value of the difference is
+    minimal
+     */
+
+    public static int min(BST bst, int number) {
+
+        return 0;
+    }
+}
---
+++ src/companies/google/mitbbs/NGram.java
@@ -0,0 +1,27 @@
+package companies.google.mitbbs;
+
+import utils.Utils;
+
+import java.util.List;
+
+/**
+ * @author: mizhang
+ * @since: Oct 31, 2013 4:53:35 PM
+ */
+public class NGram {
+    /*
+    Given a string and a integer number N, output all the k-gram of the string with k <= N.
+    For example, "hello world all" and 2, return "hell0", "world", "all", "hello world", "world all"
+     */
+    public static List<String> getNGram(String words, int n) {
+        return null;
+    }
+
+    private static void test() {
+        Utils.printList(getNGram("hello world all", 2));
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
