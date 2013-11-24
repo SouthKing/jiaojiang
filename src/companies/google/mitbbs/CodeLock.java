d6101694d10deecdf61731762d925b8afc49f62d
gatechang <gatechang@gmail.com>
2013-11-23 22:16:56 -0500

    20131122

---
+++ src/companies/google/mitbbs/CodeLock.java
@@ -0,0 +1,42 @@
+package companies.google.mitbbs;
+
+/**
+ * @author: mizhang
+ * @since: Nov 22, 2013 1:10:02 PM
+ */
+public class CodeLock {
+    /*
+     A digital door lock with a 4-digit code and therefore there are totally 10000 combinations. Need to find a shorted
+     string that all these 10000 combinations are exists in the string.
+
+     For example. assume you have a 2-digit code and each digit ranges from 0 to 1, then you have 4 different combinations:
+     00, 01, 10, 11 and 0011 contains all the combinations (from last to first is 10)
+
+     It is also called De Bruijn sequence.
+     */
+    public static String getSequence() {
+        return null;
+    }
+
+    public static void getHamiltonianCycle() {
+        
+    }
+
+    // assume i and j are in the range of [0, 10000]
+    private static boolean isPath(int i, int j) {
+        return i % 1000 == j / 10;
+    }
+
+    private int[] getNeighbors(int i) {
+        int[] ret = new int[10];
+        for (int ii = 0; ii < ret.length; ii++) {
+            ret[ii] = (i % 1000) * 10 + i;
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSequence());
+    }
+}
---
+++ src/companies/google/mitbbs/LastThousandDigits.java
@@ -0,0 +1,18 @@
+package companies.google.mitbbs;
+
+/**
+ * @author: mizhang
+ * @since: Nov 22, 2013 4:12:45 PM
+ */
+public class LastThousandDigits {
+    /*
+    Find the last 1000 digits of 5^1234566789893943
+     */
+    public static String last1000Digits() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(last1000Digits());
+    }
+}
