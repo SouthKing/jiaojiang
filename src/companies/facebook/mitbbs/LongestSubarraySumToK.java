0d910c7ff6d8c89484c6a67ac1fad58b047a4b9d
gatechang <gatechang@gmail.com>
2013-12-05 22:48:56 -0500

    rename

--- src/companies/facebook/mitbbs/LongestSubarraySumToK.java
+++ src/companies/facebook/mitbbs/LongestSubarraySumToK.java
@@ -2,6 +2,9 @@ package companies.facebook.mitbbs;
 
 import utils.Utils;
 
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * @author: zhang
  * @since: Nov 19, 2013 10:48:17 PM
@@ -29,10 +32,36 @@ public class LongestSubarraySumToK {
         return length;
     }
 
+    /*
+    in case negative number
+     */
+    public static int longestSubarray2(int[] data, int target) {
+        Map<Long, Integer> map = new HashMap<Long, Integer>();
+        long sum = 0;
+        int maxLength = 0;
+        for (int i = 0; i < data.length; i++) {
+            sum += data[i];
+            if (map.containsKey(sum - target)) {
+                maxLength = Math.max(maxLength, i - map.get(sum - target));
+            }
+
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return maxLength;
+    }
+
     private static void test() {
         Utils.printTestln(longestSubarray(new int[]{1, 3, 2, 8, 5, 1, 1, 1, 1, 2}, 6), 5);
         Utils.printTestln(longestSubarray(new int[]{1, 3, 2, 8, 5, 1, 1, 1, 1, 2}, 16), 5);
         Utils.printTestln(longestSubarray(new int[]{1, 3, 2, 8, 5, 1, 1, 1, 1, 2}, 116), 0);
+        Utils.printTestln(longestSubarray(new int[]{1, 3, 2, 8, 5, -1, -1, -1, -1, 2}, 16), 9);
+        System.out.println("-------------------");
+        Utils.printTestln(longestSubarray2(new int[]{1, 3, 2, 8, 5, 1, 1, 1, 1, 2}, 6), 5);
+        Utils.printTestln(longestSubarray2(new int[]{1, 3, 2, 8, 5, 1, 1, 1, 1, 2}, 16), 5);
+        Utils.printTestln(longestSubarray2(new int[]{1, 3, 2, 8, 5, 1, 1, 1, 1, 2}, 116), 0);
+        Utils.printTestln(longestSubarray2(new int[]{1, 3, 2, 8, 5, -1, -1, -1, -1, 2}, 16), 9);
     }
 
     public static void main(String[] args) {
--- src/companies/facebook/mitbbs/TreePreorderTraversal.java
+++ src/companies/facebook/mitbbs/TreePreorderTraversal.java
@@ -1,6 +1,7 @@
 package companies.facebook.mitbbs;
 
-import utils.TreeNode;
+import utils.NTreeNode;
+import jiaojiang.leetcode.onlinejudge.TreeNode;
 
 /**
  * @author: mizhang
@@ -10,9 +11,31 @@ public class TreePreorderTraversal {
     /*
     Given a regular tree, write the iterative preorder traversal algorithm
      */
-    public static void preorder(TreeNode root) {
+    public static void preorder(NTreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.value + "\t");
+        if (root.children == null) {
+            return;
+        }
+        for (NTreeNode node : root.children) {
+            preorder(node);
+        }
+    }
+
+    public static void test() {
+        NTreeNode root = NTreeNode.constructSampleTree();
+        preorder(root);
+
+        preorderBinaryTree(TreeNode.construct());
+    }
+
+    public static void preorderBinaryTree(TreeNode root) {
         
     }
 
-
+    public static void main(String[] args) {
+        test();
+    }
 }
