27571466095b49c8d7b0c78a51d75706d38123a1
gatechang <gatechang@gmail.com>
2013-12-12 23:24:40 -0500

    palindrome partition

---
+++ src/jiaojiang/leetcode2/onlinejudge/PalindromePartitionII.java
@@ -0,0 +1,28 @@
+package jiaojiang.leetcode2.onlinejudge;
+
+/**
+ * @author: mizhang
+ * @since: Nov 7, 2013 4:18:04 PM
+ */
+public class PalindromePartitionII {
+    /*
+     Given a string s, partition s such that every substring of the partition is a palindrome.
+
+    Return the minimum cuts needed for a palindrome partitioning of s.
+
+    For example, given s = "aab",
+    Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+     */
+    public int minCut(String s) {
+        return 0;
+    }
+
+    private static void test() {
+
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+
+}
