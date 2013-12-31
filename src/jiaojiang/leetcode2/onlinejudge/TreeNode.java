a8b243e22b380a51565e2a2e06cd198482002d04
gatechang <gatechang@gmail.com>
2013-12-31 17:01:27 -0500

    min path sum

---
+++ src/jiaojiang/leetcode2/onlinejudge/TreeNode.java
@@ -0,0 +1,61 @@
+package jiaojiang.leetcode2.onlinejudge;
+
+/**
+ * @author: mizhang
+ * @since: Dec 31, 2013 3:15:13 PM
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public static void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + "\t");
+        inOrder(root.right);
+    }
+
+    public static void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + "\t");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+
+    /*
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+     */
+    public static TreeNode getSampleTree() {
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = getSampleTree();
+        inOrder(root);
+        System.out.println();
+        preOrder(root);
+    }
+}
