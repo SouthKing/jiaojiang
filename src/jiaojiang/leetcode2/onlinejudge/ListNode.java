58af44f46b74408d63b7121004133dda5b375b63
gatechang <gatechang@gmail.com>
2014-01-05 01:52:39 -0500

    20140104

---
+++ src/jiaojiang/leetcode2/onlinejudge/ListNode.java
@@ -0,0 +1,14 @@
+package jiaojiang.leetcode2.onlinejudge;
+
+/**
+ * @author: zhang
+ * @since: Jan 5, 2014 1:51:18 AM
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/jiaojiang/leetcode2/onlinejudge/ReverseNodeInKGroup.java
@@ -0,0 +1,38 @@
+package jiaojiang.leetcode2.onlinejudge;
+
+/**
+ * @author: zhang
+ * @since: Jan 5, 2014 1:50:03 AM
+ */
+public class ReverseNodeInKGroup {
+    /*
+    Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+    If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+    You may not alter the values in the nodes, only nodes itself may be changed.
+
+    Only constant memory is allowed.
+
+    For example,
+    Given this linked list: 1->2->3->4->5
+
+    For k = 2, you should return: 2->1->4->3->5
+
+    For k = 3, you should return: 3->2->1->4->5
+     */
+    public static ListNode reverseKGroup(ListNode head, int k) {
+
+        return null;
+    }
+
+    private static void test() {
+
+    }
+
+    public static void main(String[] args){
+        test();
+    }
+
+
+}
