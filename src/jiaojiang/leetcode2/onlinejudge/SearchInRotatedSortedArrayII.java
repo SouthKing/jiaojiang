2c6f8dc0d446a0a52794e6ca212dcf844e54aaa3
gatechang <gatechang@gmail.com>
2014-01-14 23:00:17 -0500

    20140114

--- src/jiaojiang/leetcode2/onlinejudge/SearchInRotatedSortedArrayII.java
+++ src/jiaojiang/leetcode2/onlinejudge/SearchInRotatedSortedArrayII.java
@@ -19,51 +19,41 @@ public class SearchInRotatedSortedArrayII {
         int start = 0, end = A.length - 1, mid;
 
         while (start <= end) {
+            while (start < end && A[start] == A[end]) {
+                start++;
+            }
             mid = (start + end) >>> 1;
 
-            if (A[mid] == target) {
+            if (A[mid] == target || A[start] == target || A[end] == target) {
                 return true;
             }
 
             if (A[mid] < target) {
-                if (A[mid] > A[start]) {
+                if (A[start] < A[end] || target < A[end] || A[mid] > A[end]) {
                     start = mid + 1;
                 } else {
-                    if (target == A[end]) {
-                        return true;
-                    }
-                    if (target < A[end]) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
+                    end = mid - 1;
                 }
             } else {
-                if (A[mid] < A[start]) {
+                if (A[start] < A[end] || target > A[end] || A[mid] <= A[end]) {
                     end = mid - 1;
                 } else {
-                    if (target == A[start])  {
-                        return true;
-                    }
-                    if (target > A[start]) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
+                    start  = mid + 1;
                 }
             }
         }
-
         return false;
     }
 
     private static void test() {
-        Utils.printTestln(search(new int[]{1,1,3,1}, 2), false);
-        Utils.printTestln(search(new int[]{1,1,3,1}, 3), true);
-        Utils.printTestln(search(new int[]{4,5,6,7,0,1,2}, 0), true);
-        Utils.printTestln(search(new int[]{3,4,5,1, 2}, 3), true);
-        Utils.printTestln(search(new int[]{3,4,5,1, 2}, 2), true);
-        Utils.printTestln(search(new int[]{3,4,5,1, 2}, 11), false);
+//        Utils.printTestln(search(new int[]{1,1,3,1}, 2), false);
+//        Utils.printTestln(search(new int[]{1,1,3,1}, 2), false);
+//        Utils.printTestln(search(new int[]{0,0,1,1,2,0}, 2), true);
+        Utils.printTestln(search(new int[]{1,2,0,1,1,1}, 0), true);
+//        Utils.printTestln(search(new int[]{4,5,6,7,0,1,2}, 0), true);
+//        Utils.printTestln(search(new int[]{3,4,5,1, 2}, 3), true);
+//        Utils.printTestln(search(new int[]{3,4,5,1, 2}, 2), true);
+//        Utils.printTestln(search(new int[]{3,4,5,1, 2}, 11), false);
     }
 
     public static void main(String[] args) {
