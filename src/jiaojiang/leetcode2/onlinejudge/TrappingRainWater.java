21a85ec3587858f12fd2f46fa9437f5903ac738c
gatechang <gatechang@gmail.com>
2014-01-19 01:11:18 -0500

    20140118

--- src/jiaojiang/leetcode2/onlinejudge/TrappingRainWater.java
+++ src/jiaojiang/leetcode2/onlinejudge/TrappingRainWater.java
@@ -20,11 +20,28 @@ public class TrappingRainWater {
     (blue section) are being trapped. Thanks Marcos for contributing this image!
      */
     public static int trap(int[] A) {
-        return 0;
+        if (A.length < 2) {
+            return 0;
+        }
+        
+        int[] rightMax = new int[A.length - 2];
+        rightMax[rightMax.length - 1] = A[A.length - 1];
+        for (int i = rightMax.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], A[i + 2]);
+        }
+
+        int water = 0, leftMax = A[0];
+        for (int i = 1; i < A.length - 1; i++) {
+            water += Math.max(0, Math.min(leftMax, rightMax[i - 1]) - A[i]);
+            leftMax = Math.max(A[i], leftMax);
+        }
+
+        return water;
     }
 
     private static void test() {
         Utils.printTestln(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}), 6);
+        Utils.printTestln(trap(new int[0]), 0);
     }
 
     public static void main(String[] args) {
