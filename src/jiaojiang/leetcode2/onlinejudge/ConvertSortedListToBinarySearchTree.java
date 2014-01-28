b6599439bde912bf1b3a0797927b2fecb4ded55d
gatechang <gatechang@gmail.com>
2014-01-27 22:27:48 -0500

    20140127

--- src/jiaojiang/leetcode2/onlinejudge/ConvertSortedListToBinarySearchTree.java
+++ src/jiaojiang/leetcode2/onlinejudge/ConvertSortedListToBinarySearchTree.java
@@ -1,5 +1,9 @@
 package jiaojiang.leetcode2.onlinejudge;
 
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * @author: zhang
  * @since: Jan 26, 2014 10:33:16 PM
@@ -9,7 +13,32 @@ public class ConvertSortedListToBinarySearchTree {
     Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
      */
     public static TreeNode sortedListToBST(ListNode head) {
+        int length;
+        ListNode node;
 
+        for (length = 0, node = head; node != null; node = node.next, length++);
+
+        ListNode[] nodes = new ListNode[1];
+        nodes[0] = head;
+        
+        return sortedLlistToBST(nodes, 0, length);
+    }
+
+    public static TreeNode sortedLlistToBST(ListNode[] head, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) >>> 1;
+
+        TreeNode left = sortedLlistToBST(head, start, mid - 1);
+        TreeNode p = new TreeNode(head[0].val);
+        head[0] = head[0].next;
+        TreeNode right = sortedLlistToBST(head, mid + 1, end);
+        p.left = left;
+        p.right = right;
+
+        return p;
     }
 
     private static void test() {
