60da1e9bd019aa53e69f9c88e17337884711674c
gatechang <gatechang@gmail.com>
2014-01-30 23:58:32 -0500

    20140130

--- src/jiaojiang/leetcode/onlinejudge/GasStation.java
+++ src/jiaojiang/leetcode/onlinejudge/GasStation.java
@@ -43,12 +43,12 @@ public class GasStation {
     }
 
     public static void test() {
-//        Utils.printTestln(canCompleteCircuit(new int[]{4}, new int[]{5}), -1);
+        Utils.printTestln(canCompleteCircuit(new int[]{4}, new int[]{5}), -1);
         Utils.printTestln(canCompleteCircuit(new int[]{5}, new int[]{4}), 0);
-//        Utils.printTestln(canCompleteCircuit(new int[]{1, 2}, new int[]{2, 1}), 1);
-//        Utils.printTestln(canCompleteCircuit(new int[]{2, 3, 1}, new int[]{3, 1, 2}), 1);
-//        Utils.printTestln(canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}), 3);
-//        Utils.printTestln(canCompleteCircuit(new int[]{6,0,1,3,2}, new int[]{4,5,2,5,5}), -1);
+        Utils.printTestln(canCompleteCircuit(new int[]{1, 2}, new int[]{2, 1}), 1);
+        Utils.printTestln(canCompleteCircuit(new int[]{2, 3, 1}, new int[]{3, 1, 2}), 1);
+        Utils.printTestln(canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}), 3);
+        Utils.printTestln(canCompleteCircuit(new int[]{6,0,1,3,2}, new int[]{4,5,2,5,5}), -1);
     }
 
     public static void main(String[] args) {
